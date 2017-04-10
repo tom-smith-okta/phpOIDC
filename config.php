255ba7bed0088dddbe1cbd146fc36a83d5c2fa2d
Tom Smith <tom.smith@okta.com>
2017-04-10 15:07:59 -0400

    moving client secret out of repo

--- config.php
+++ config.php
@@ -3,9 +3,26 @@
 // Config
 $config["oktaOrg"] = "https://tomco.okta.com/";
 $config["client_id"] = "SV3N5CnEAMkOV4tUkIs6";
-$config["client_secret"] = "Byw_25k3FHrtpbn21LlZgsk_GMnsXIABV0ed7Fju";
+// $config["client_secret"] = "Byw_25k3FHrtpbn21LlZgsk_GMnsXIABV0ed7Fju";
 $config["oauthURL"] = $config["oktaOrg"] . "/oauth2/v1/";
-$config["redirect_uri"] = "http://localhost:8888/oidcPHP/redirect.php";
+// $config["redirect_uri"] = "http://localhost:8888/oidcPHP/redirect.php";
+
+$localKey = "/Users/tomsmith/keys/vishalSecret.txt";
+$remoteKey = "/usr/local/keys/vishalSecret.txt";
+
+if (file_exists($localKey)) {
+	$config["client_secret"] = file_get_contents($localKey);
+	$config["redirect_uri"] = "http://localhost:8888/oidcPHP/redirect.php";
+}
+else {
+	$config["client_secret"] = file_get_contents($remoteKey);
+	$config["redirect_uri"] = "http://vishal.atkdemo.com/redirect.php";
+}
+
+echo "<p>the client secret is: " . $config["client_secret"];
+echo "<p>the redirect URL is: " . $config["redirect_uri"];
+
+exit;
 
 // $config["redirect_uri"] = getRedirect_uri();
 
