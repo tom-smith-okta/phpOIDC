24b212de8d7987e2e8cf12a771f25c653e093fbb
Tom Smith <tom.smith@okta.com>
2017-04-12 12:23:39 -0400

    more visual UI

--- page02.php
+++ page02.php
@@ -6,16 +6,13 @@ if (session_status() === PHP_SESSION_NONE) { session_start(); }
 include "oktaAuthN.php";
 include "utils.php";
 
-$thisPage = "index.php";
+$thisPage = "page02.php";
 
 if (isAuthenticated()) {
 	showContent($thisPage);
 }
 else {
-	$url = getOauthURL("index.php");
-	$output = "<p>the user is not authenticated.</p>";
-	$output .= "<p>click <a href = '$url'>here</a> to authenticate.</p>";
-	showPage($output);
+	authenticate($thisPage);
 }
 
 exit;
---
+++ utils.php
@@ -0,0 +1,50 @@
+<?php
+
+function showContent($pageName) {
+	$token_info = json_decode($_SESSION["token_info"]);
+	$userName = $token_info->preferred_username;
+
+	$output .= "<p>the user is authenticated.</p>";
+	$output .= "<p>Welcome, " . $userName . "!</p>";
+
+	$output .= "<p>the page is: " . $pageName . "</p>";
+
+	showPage($output);
+}
+
+function showPage($output) {
+
+	$page = file_get_contents("html/template.html");
+
+	$page = str_replace("%--authnstatus--%", $output, $page);
+
+	echo $page;
+
+	exit;
+
+}
+
+// not for prod
+// generates redirect_uri dynamically
+function getRedirect_uri() {
+
+	// http or https
+	if (isSecure()) { $protocol = "https"; }
+	else { $protocol = "http"; }
+
+	$redirectURI = $protocol . "://" . $_SERVER["SERVER_NAME"];
+
+	// add the port to the hostname if appropriate
+	if (array_key_exists("SERVER_PORT", $_SERVER)) {
+		if ($_SERVER["SERVER_PORT"] == "80") {}
+		else { $redirectURI .= ":" . $_SERVER["SERVER_PORT"]; }
+	}
+
+	return $redirectURI . $_SERVER["PHP_SELF"];
+}
+
+function isSecure() {
+	return
+		(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+		|| $_SERVER['SERVER_PORT'] == 443;
+}
